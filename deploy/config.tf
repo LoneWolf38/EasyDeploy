f9b1b5294bdfa4b68b19242870784cc0281b4d71
LoneWolf38 <sudodeba97.iiitbbsr@gmail.com>
2018-12-09 13:16:05 +0000

    Variables

--- deploy/config.tf
+++ deploy/config.tf
@@ -1,1 +1,48 @@
-#Servers and Users data
+#Variables declaration 
+
+variable "access_key" {
+  description = "AWS Access Key"
+}
+
+variable "secret_key" {
+  description = "AWS Secret Key"
+}
+
+variable "instance_name" {
+  description = "Aws instance name"
+}
+
+variable "private_key" {
+  description = "Private key used for login"
+}
+
+variable "region" {
+  description = "Region where the AWS instance will be created"
+  default     = "ap-south-1"                                    #For mine
+}
+
+variable "ami_per_region" {
+  description = "AMI available for regions"
+  type        = "map"
+
+  default = {
+    "ap-south-1" = "ami-0d773a3b7bb2bb1c1"
+  }
+}
+
+variable "instance_type" {
+  description = "Type of instance as per the projects"
+  type        = "map"
+}
+
+variable "firewall" {
+  description = "Security Groups as per the app"
+  type        = "map"
+
+  default = {
+    "ssh_only"       = ["22"]
+    "http_only"      = ["22", "80"]
+    "https_only"     = ["22", "443"]
+    "static_website" = ["22", "80", "443"]
+  }
+}
