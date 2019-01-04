45f7486043f516849759fd95e1b26aeec84f276c
LoneWolf38 <sudodeba97.iiitbbsr@gmail.com>
2019-01-04 00:20:20 +0000

    Code Clean

--- main.go
+++ main.go
@@ -5,7 +5,7 @@ import (
 	//"log"
 
 	"os"
-	//"github.com/LoneWolf38/EasyDeploy/cmd"
+	"github.com/LoneWolf38/EasyDeploy/cmd"
 	"github.com/LoneWolf38/EasyDeploy/provisioner"
 )
 
@@ -14,6 +14,6 @@ import (
 
 func main() {
 
-	//cmd.RootCmd.Execute()
+	cmd.RootCmd.Execute()
 	provisioner.StaticDeploy("linkto the projects",os.Getenv("HOME"))
 }
